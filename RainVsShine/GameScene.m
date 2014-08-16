9da34f31cd098084d07bb2fd9b54d04d8fd4c3c6
ybrikeeg <kirbygee@stanford.edu>
2014-08-15 18:13:10 -0700

    Clouds run

--- RainVsShine/GameScene.m
+++ RainVsShine/GameScene.m
@@ -8,32 +8,47 @@
 
 #import "GameScene.h"
 
+#define STATUS_BAR_HEIGHT 20
+#define CLOUD_X_OFFSET 50
+@interface GameScene ()
+@property (nonatomic, strong) NSMutableArray *cloudArray;
+@end
 @implementation GameScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-
+       self.backgroundColor = [UIColor colorWithRed:.5 green:.7 blue:.3 alpha:1.0f];
+      [self initializeClouds];
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+- (void)initializeClouds
+{
+   self.cloudArray = [[NSMutableArray alloc] init];
+   SKAction *wait = [SKAction waitForDuration:.1];
+   SKAction *performSelector = [SKAction performSelector:@selector(createCloud) onTarget:self];
+   SKAction *sequence = [SKAction sequence:@[performSelector, wait]];
+   SKAction *repeat   = [SKAction repeatActionForever:sequence];
+   [self runAction:repeat];
+   
 }
+
+- (void)createCloud
+{
+   SKSpriteNode *cloud = [SKSpriteNode spriteNodeWithImageNamed:@"cloud"];
+   cloud.position = CGPointMake(-CLOUD_X_OFFSET, self.view.bounds.size.height - arc4random()%200 - STATUS_BAR_HEIGHT - cloud.frame.size.height/2);
+   [self addChild:cloud];
+
+   [self.cloudArray addObject:cloud];
+   
+   SKAction *move = [SKAction moveToX:self.view.bounds.size.width duration:arc4random()%100 / 100 + 3.0];
+   [cloud runAction:move completion:^{
+      [self.cloudArray removeObject:cloud];
+      [cloud removeFromParent];
+   }];
+}
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
